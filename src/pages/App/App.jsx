15bc5332d6b2f7bc933662ce2d300d9c7c823199
Fesomibe <francisesomibe1@gmail.com>
2024-02-27 00:40:09 -0600

    title and components

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,29 +1,27 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { getUser } from '../../utilities/users-service';
-import './App.css';
-import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from '../../components/Budget/Budget';
+import Remaining from '../../components/Remaining/Remaining';
+import ExpenseTotal from '../../components/ExpenseTotal/ExpenseTotal';
 
-export default function App() {
-  const [user, setUser] = useState(getUser());
 
-  return (
-    <main className="App">
-      { user ?
-          <>
-            <NavBar user={user} setUser={setUser} />
-            <Routes>
-              {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
-            </Routes>
-          </>
-          :
-          <AuthPage setUser={setUser} />
-      }
-    </main>
-  );
-}
+const App = () => {
+	return (
+			<div className='container'>
+				<h1 className='mt-3'>My Budget Planner</h1>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<Budget />
+					</div>
+					<div className='col-sm'>
+						<Remaining />
+					</div>
+					<div className='col-sm'>
+						<ExpenseTotal />
+					</div>
+				</div>
+			</div>
+	);
+};
+
+export default App;
